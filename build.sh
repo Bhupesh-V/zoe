cb255fc17c0b00e7fe890450d2bf970ba4d7059d
Bhupesh-V <varshneybhupesh@gmail.com>
2020-10-31 18:58:30 +0530

    better build script

--- build.sh
+++ build.sh
@@ -3,23 +3,21 @@
 package_name="zoe"
 platforms=("windows/amd64" "linux/amd64" "linux/arm64" "linux/386" "darwin/amd64")
 
+# remove any old binaries
+rm "$package_name"-*
+
 for platform in "${platforms[@]}"
 do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name="$package_name-$GOOS-$GOARCH"
-    echo -e "Building for $GOOS-$GOARCH"
-    if [ "$GOOS" = "windows" ]; then
-        output_name+='.exe'
-    elif [ "$GOOS" = "darwin" ]; then
-    	output_name="$package_name-macos-$GOARCH"
-    fi
-
-    if env GOOS="$GOOS" GOARCH="$GOARCH" go build -o "$output_name"; then
-    	echo -e "Built zoe for $platform ✅"
+    printf "%s " "Building for $GOOS-$GOARCH"
+    if export GOOS="$GOOS" GOARCH="$GOARCH"; then
+        output_name="$package_name-$GOOS-$GOARCH$(go env GOEXE)"
+        go build -o "$output_name"
+        echo -e "✅"
     else
-    	echo -e "An error has occurred! Aborting ..."
+        echo -e "An error has occurred! Aborting ..."
         exit 1
     fi
 done
